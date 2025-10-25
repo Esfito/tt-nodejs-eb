0f9028909b0c6fb429447ee79f443829de0ed208
Esfito <103140553+Esfito@users.noreply.github.com>
2025-10-24 22:15:12 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Talento Tech - Nodejs - Ezequiel Bilsky
+# Pre entrega para el cusro de NodeJs, incluye métodos de CRUD vistos en clase con la Fake Api Store.
---
+++ index.js
@@ -0,0 +1,76 @@
+let [, , meth, rsrce, ...params] = process.argv;
+
+meth = meth.toUpperCase();
+rsrce = rsrce.toLowerCase();
+
+
+//CREATE
+if (meth == "POST" && rsrce == "products") {
+    const [title, price, category] = params;
+    const product = {
+        title,
+        price,
+        category,
+    };
+    const response = await fetch("https://fakestoreapi.com/products", {
+        meth: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(product),
+    });
+    const data = await response.json();
+    console.log(data);
+}
+
+
+//READ
+//GET all
+if (meth == "GET" && rsrce == "products") {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+    console.log(data);
+}
+
+//GET by id
+if (meth == "GET" && rsrce.startsWith("products/")) {
+  let id = rsrce.split("/")[1];
+  id = parseInt(id);
+
+  if (isNaN(id) || id <= 0) {
+    console.log("No es un numero");
+  }
+  
+  const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+  const data = await response.json();
+  console.log(data);
+}
+
+
+//UPDATE
+if (meth == "PUT" && rsrce.startsWith("products/")) {
+    let id = rsrce.split("/")[1];
+    id = parseInt(id);
+    const [title, price, category] = params;
+    const product = {
+        title,
+        price,
+        category,
+    };
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
+        meth: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(product),
+    });
+    const data = await response.json();
+    console.log(data);
+}
+
+
+//DELETE
+if (meth == "DELETE" && rsrce.startsWith("products/")) {
+    const id = parseInt(rsrce.split("/")[1]);
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
+        meth: "DELETE"
+    });
+    const data = await response.json();
+    console.log(data);
+}
